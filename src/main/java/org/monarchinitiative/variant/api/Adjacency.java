23e29bb6dd1663effb2e8d9b023339436f7f61d7
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-11-10 20:29:56 +0000

    Add new Adjacency interface

---
+++ src/main/java/org/monarchinitiative/variant/api/Adjacency.java
@@ -0,0 +1,15 @@
+package org.monarchinitiative.variant.api;
+
+/**
+ * Adjacency ties together two breakends, as described in VCF specs.
+ */
+public interface Adjacency extends Breakended, Stranded<Adjacency> {
+
+    @Override
+    default Strand getStrand() {
+        return getLeft().getStrand();
+    }
+
+    @Override
+    Adjacency toOppositeStrand();
+}
