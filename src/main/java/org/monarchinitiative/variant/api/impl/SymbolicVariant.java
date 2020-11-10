427c278af250876543a13357e176e56daf21f08c
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-11-10 21:24:36 +0000

    Add new SymbolicVariant implementation

---
+++ src/main/java/org/monarchinitiative/variant/api/impl/SymbolicVariant.java
@@ -0,0 +1,148 @@
+package org.monarchinitiative.variant.api.impl;
+
+import org.monarchinitiative.variant.api.*;
+
+import java.util.Objects;
+
+/**
+ * Represents a large (or small)
+ */
+public final class SymbolicVariant implements Variant {
+
+    private final Contig contig;
+    private final Strand strand;
+    private final Position startPosition;
+    private final Position endPosition;
+    private final String ref;
+    private final String alt;
+    private final int length;
+    private final VariantType variantType;
+
+    public SymbolicVariant(Contig contig, Strand strand, Position startPosition, Position endPosition, String ref, String alt, int length, VariantType variantType) {
+        if (!VariantType.isLargeSymbolic(alt)) {
+            throw new IllegalArgumentException("Unable to create symbolic variant from non-symbolic or breakend allele " + alt);
+        }
+        this.contig = Objects.requireNonNull(contig);
+        this.strand = Objects.requireNonNull(strand);
+        this.startPosition = Objects.requireNonNull(startPosition);
+        this.endPosition = Objects.requireNonNull(endPosition);
+        this.ref = Objects.requireNonNull(ref);
+        this.alt = Objects.requireNonNull(alt);
+        // check length agrees with start / end type
+        this.length = length;
+        // check type agrees with alt
+        this.variantType = Objects.requireNonNull(variantType);
+    }
+
+    /**
+     * @return precise one-based, positive strand symbolic variant
+     */
+    public static SymbolicVariant of(Contig contig, int start, int end, String ref, String alt, int length, VariantType variantType) {
+        return of(contig, Position.of(start), Position.of(end), ref, alt, length, variantType);
+    }
+
+    /**
+     * @return one-based, positive strand symbolic variant
+     */
+    public static SymbolicVariant of(Contig contig, Position startPosition, Position endPosition, String ref, String alt, int length, VariantType variantType) {
+        return new SymbolicVariant(contig, Strand.POSITIVE, startPosition, endPosition, ref, alt, length, variantType);
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public Contig getContig() {
+        return contig;
+    }
+
+    @Override
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+    @Override
+    public Position getEndPosition() {
+        return endPosition;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public String getRef() {
+        return ref;
+    }
+
+    @Override
+    public String getAlt() {
+        return alt;
+    }
+
+    @Override
+    public VariantType getType() {
+        return variantType;
+    }
+
+    @Override
+    public Strand getStrand() {
+        return strand;
+    }
+
+    @Override
+    public SymbolicVariant withStrand(Strand strand) {
+        if (this.strand == strand) {
+            return this;
+        } else {
+            Position start = Position.of(contig.getLength() - startPosition.getPos() + 1,
+                    startPosition.getConfidenceInterval().toOppositeStrand());
+            // TODO broken with ins/del need to use length which should be +/-
+            Position end = Position.of(contig.getLength() - endPosition.getPos() + 1,
+                    endPosition.getConfidenceInterval().toOppositeStrand());
+            return new SymbolicVariant(contig, strand, start, end, Seq.reverseComplement(ref), alt, length, variantType);
+        }
+    }
+
+    @Override
+    public boolean isSymbolic() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SymbolicVariant)) return false;
+        SymbolicVariant that = (SymbolicVariant) o;
+        return length == that.length &&
+                contig.equals(that.contig) &&
+                strand == that.strand &&
+                startPosition.equals(that.startPosition) &&
+                endPosition.equals(that.endPosition) &&
+                ref.equals(that.ref) &&
+                alt.equals(that.alt) &&
+                variantType == that.variantType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contig, strand, startPosition, endPosition, length, ref, alt, variantType);
+    }
+
+    @Override
+    public String toString() {
+        return "SymbolicVariant{" +
+                "contig=" + contig.getId() +
+                ", strand=" + strand +
+                ", startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                ", length=" + length +
+                ", ref='" + ref + '\'' +
+                ", alt='" + alt + '\'' +
+                ", variantType=" + variantType +
+                '}';
+    }
+}
---
+++ src/test/java/org/monarchinitiative/variant/api/impl/SymbolicVariantTest.java
@@ -0,0 +1,107 @@
+package org.monarchinitiative.variant.api.impl;
+
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.variant.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ */
+class SymbolicVariantTest {
+
+    private final Contig chr1 = Contig.of(1, "1", SequenceRole.ASSEMBLED_MOLECULE, 1000, "", "", "chr1");
+
+    @Test
+    void symbolicVariantOverlapsOther() {
+        Variant largeIns = SymbolicVariant.of(chr1, 1, 100, "T", "<INS>", 100, VariantType.INS);
+        Variant otherIns = SymbolicVariant.of(chr1, 99, 299, "C", "<INS>", 200, VariantType.INS);
+        assertTrue(largeIns.overlapsWith(otherIns));
+        assertTrue(otherIns.overlapsWith(largeIns));
+    }
+
+
+    @Test
+    void throwsIllegalArgumentWithNonSymbolicAllele() {
+        // this ought to be legal, but maybe only when called on the interface using Variant.of(...) which defers to the correct implementation
+        assertThrows(IllegalArgumentException.class, () -> SymbolicVariant.of(chr1, 1, 1, "A", "T", 1, VariantType.SNV));
+    }
+
+    @Test
+    void symbolicThrowsIllegalArgumentWithBreakendAllele() {
+        assertThrows(IllegalArgumentException.class, () -> SymbolicVariant.of(chr1, 1, 1, "A", "A[1:2]", 1, VariantType.BND));
+    }
+
+    @Test
+    void shouldBeSymbolic() {
+        Variant instance = SymbolicVariant.of(chr1, 1, 1, "A", "<INS>", 100, VariantType.INS);
+        assertThat(instance.isSymbolic(), equalTo(true));
+    }
+
+    @Test
+    void symbolicDel() {
+        Variant del = SymbolicVariant.of(chr1, 1, 100, "A", "<DEL>", 100, VariantType.DEL);
+
+        assertThat(del.getContig(), equalTo(chr1));
+        assertThat(del.getStartPosition(), equalTo(Position.of(1)));
+        assertThat(del.getEndPosition(), equalTo(Position.of(100)));
+        assertThat(del.getType(), equalTo(VariantType.DEL));
+        assertThat(del.getLength(), equalTo(100));
+        assertThat(del.getRef(), equalTo("A"));
+        assertThat(del.getAlt(), equalTo("<DEL>"));
+    }
+
+    @Test
+    void symbolicIns() {
+        Variant ins = SymbolicVariant.of(chr1, 1, 1, "A", "<INS>", 100, VariantType.INS);
+
+        assertThat(ins.getContig(), equalTo(chr1));
+        assertThat(ins.getStartPosition(), equalTo(Position.of(1)));
+        assertThat(ins.getEndPosition(), equalTo(Position.of(1)));
+        assertThat(ins.getType(), equalTo(VariantType.INS));
+        assertThat(ins.getLength(), equalTo(100));
+        assertThat(ins.getRef(), equalTo("A"));
+        assertThat(ins.getAlt(), equalTo("<INS>"));
+    }
+
+    @Test
+    void symbolicInsWithSameStrand() {
+        Variant ins = SymbolicVariant.of(chr1, 1, 1, "A", "<INS>", 100, VariantType.INS);
+        assertSame(ins, ins.withStrand(Strand.POSITIVE));
+    }
+
+    @Test
+    void symbolicInsWithNegativeStrand() {
+        Variant ins = SymbolicVariant.of(chr1, 1, 1, "A", "<INS>", 100, VariantType.INS);
+        Variant negativeIns = ins.withStrand(Strand.NEGATIVE);
+
+        assertThat(negativeIns.getContig(), equalTo(chr1));
+        assertThat(negativeIns.getStrand(), equalTo(Strand.NEGATIVE));
+        assertThat(negativeIns.getStartPosition(), equalTo(Position.of(1000)));
+        assertThat(negativeIns.getEndPosition(), equalTo(Position.of(1000)));
+        assertThat(negativeIns.getRef(), equalTo("T"));
+        assertThat(negativeIns.getAlt(), equalTo("<INS>"));
+        assertThat(negativeIns.getType(), equalTo(VariantType.INS));
+        assertThat(negativeIns.getLength(), equalTo(100));
+    }
+
+    @Test
+    void symbolicDelWithNegativeStrand() {
+        Variant del = SequenceVariant.of(chr1, 1, "ATCATTTACC", "A");
+        Variant instance = SymbolicVariant.of(chr1, 1, 100, "A", "<DEL>", 100, VariantType.DEL);
+        Variant negative = instance.withStrand(Strand.NEGATIVE);
+        System.out.println(del);
+        System.out.println(instance);
+        System.out.println(negative);
+        assertThat(negative.getContig(), equalTo(chr1));
+        assertThat(negative.getStrand(), equalTo(Strand.NEGATIVE));
+        assertThat(negative.getStartPosition(), equalTo(Position.of(1000)));
+        assertThat(negative.getEndPosition(), equalTo(Position.of(900)));
+        assertThat(negative.getRef(), equalTo("T"));
+        assertThat(negative.getAlt(), equalTo("<DEL>"));
+        assertThat(negative.getType(), equalTo(VariantType.DEL));
+        assertThat(negative.getLength(), equalTo(100));
+    }
+}
