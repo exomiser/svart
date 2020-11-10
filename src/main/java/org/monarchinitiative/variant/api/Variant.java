55a06295424f7607641d08adbea6e661b891eee9
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-11-10 20:49:21 +0000

    Update Variant with getId() Update Variant to override Stranded types to return Variant Update Variant to add boolean isSymbolic()

--- src/main/java/org/monarchinitiative/variant/api/Variant.java
+++ src/main/java/org/monarchinitiative/variant/api/Variant.java
@@ -5,6 +5,8 @@ package org.monarchinitiative.variant.api;
  */
 public interface Variant extends GenomicRegion {
 
+    String getId();
+
     /**
      * @return String with the reference allele in the variant, without common
      * suffix or prefix to reference allele.
@@ -17,8 +19,18 @@ public interface Variant extends GenomicRegion {
      */
     String getAlt();
 
+    @Override
+    Variant withStrand(Strand strand);
+
+    @Override
+    default Variant toOppositeStrand() {
+        return withStrand(getStrand().opposite());
+    }
+
     default VariantType getType() {
         return VariantType.parseAllele(getRef(), getAlt());
     }
+
+    boolean isSymbolic();
 
 }
