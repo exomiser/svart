97334766d077d29cb679a9b40119e840ccd90b2f
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-11-10 21:23:26 +0000

    Add new SequenceVariant implementation

---
+++ src/main/java/org/monarchinitiative/variant/api/impl/SequenceVariant.java
@@ -0,0 +1,135 @@
+package org.monarchinitiative.variant.api.impl;
+
+import org.monarchinitiative.variant.api.*;
+
+import java.util.Objects;
+
+/**
+ * Represents a simple genomic variation of known sequence. Here simple is defined as not being a symbolic and/or
+ * breakend re-arrangement.
+ */
+public class SequenceVariant implements Variant {
+
+    private final Contig contig;
+    private final Strand strand;
+    private final Position startPosition;
+    private final Position endPosition;
+    private final String ref;
+    private final String alt;
+    // here ought to be the breakend and adjacency bits too
+
+    public SequenceVariant(Contig contig, Strand strand, Position startPosition, Position endPosition, String ref, String alt) {
+        if (VariantType.isSymbolic(alt)) {
+            throw new IllegalArgumentException("Unable to create non-symbolic variant from symbolic or breakend allele " + alt);
+        }
+        this.contig = Objects.requireNonNull(contig);
+        this.strand = Objects.requireNonNull(strand);
+        this.startPosition = Objects.requireNonNull(startPosition);
+        this.endPosition = Objects.requireNonNull(endPosition);
+        this.ref = Objects.requireNonNull(ref);
+        this.alt = Objects.requireNonNull(alt);
+    }
+
+    public static SequenceVariant of(Contig contig, int pos, String ref, String alt) {
+        if (VariantType.isSymbolic(alt)) {
+            throw new IllegalArgumentException("Unable to create non-symbolic variant from symbolic or breakend allele " + alt);
+        }
+        Position start = Position.of(pos);
+        Position end = calculateEnd(start, ref, alt);
+        return new SequenceVariant(contig, Strand.POSITIVE, start, end, ref, alt);
+    }
+
+    private static Position calculateEnd(Position start, String ref, String alt) {
+        // SNV case
+        if ((ref.length() | alt.length()) == 1) {
+            return start;
+        }
+        return start.withPos(start.getPos() + ref.length() - 1);
+    }
+
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public Contig getContig() {
+        return contig;
+    }
+
+    @Override
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+    @Override
+    public Position getEndPosition() {
+        return endPosition;
+    }
+
+    @Override
+    public String getRef() {
+        return ref;
+    }
+
+    @Override
+    public String getAlt() {
+        return alt;
+    }
+
+    @Override
+    public Strand getStrand() {
+        return strand;
+    }
+
+    @Override
+    public SequenceVariant withStrand(Strand strand) {
+        if (this.strand == strand) {
+            return this;
+        } else {
+            Position start = Position.of(contig.getLength() - startPosition.getPos() + 1,
+                    startPosition.getConfidenceInterval().toOppositeStrand());
+            Position end = Position.of(contig.getLength() - endPosition.getPos() + 1,
+                    endPosition.getConfidenceInterval().toOppositeStrand());
+            return new SequenceVariant(contig, strand, start, end, Seq.reverseComplement(ref), Seq.reverseComplement(alt));
+        }
+    }
+
+    @Override
+    public boolean isSymbolic() {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SequenceVariant)) return false;
+        SequenceVariant that = (SequenceVariant) o;
+        return contig.equals(that.contig) &&
+                strand == that.strand &&
+                startPosition.equals(that.startPosition) &&
+                endPosition.equals(that.endPosition) &&
+                ref.equals(that.ref) &&
+                alt.equals(that.alt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contig, strand, startPosition, endPosition, ref, alt);
+    }
+
+    @Override
+    public String toString() {
+        return "SequenceVariant{" +
+                "contig=" + contig.getId() +
+                ", strand=" + strand +
+                ", startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                ", length=" + getLength() +
+                ", ref='" + ref + '\'' +
+                ", alt='" + alt + '\'' +
+                ", variantType=" + getType() +
+                '}';
+    }
+}
---
+++ src/test/java/org/monarchinitiative/variant/api/impl/SequenceVariantTest.java
@@ -0,0 +1,116 @@
+package org.monarchinitiative.variant.api.impl;
+
+import org.junit.jupiter.api.Test;
+import org.monarchinitiative.variant.api.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
+ */
+class SequenceVariantTest {
+
+    private final Contig chr1 = Contig.of(1, "1", SequenceRole.ASSEMBLED_MOLECULE, 1000, "", "", "chr1");
+
+    @Test
+    void throwsIllegalArgumentWithSymbolicAllele() {
+        assertThrows(IllegalArgumentException.class, () -> SequenceVariant.of(chr1, 1, "A", "<INS>"));
+    }
+
+    @Test
+    void throwsIllegalArgumentWithBreakendAllele() {
+        assertThrows(IllegalArgumentException.class, () -> SequenceVariant.of(chr1, 1, "A", "A[1:2]"));
+    }
+
+    @Test
+    void shouldNotBeSymbolic() {
+        Variant instance = SequenceVariant.of(chr1, 1, "A", "T");
+        assertThat(instance.isSymbolic(), equalTo(false));
+    }
+
+    @Test
+    void snv() {
+        Variant snv = SequenceVariant.of(chr1, 1, "A", "T");
+
+        assertThat(snv.getStartPosition(), equalTo(snv.getEndPosition()));
+        assertThat(snv.getType(), equalTo(VariantType.SNV));
+        assertThat(snv.getLength(), equalTo(1));
+    }
+
+    @Test
+    void mnv() {
+        Variant mnv = SequenceVariant.of(chr1, 1, "AT", "TG");
+
+        assertThat(mnv.getStartPosition(), equalTo(Position.of(1)));
+        assertThat(mnv.getStart(), equalTo(1));
+        assertThat(mnv.getEndPosition(), equalTo(Position.of(2)));
+        assertThat(mnv.getType(), equalTo(VariantType.MNV));
+        assertThat(mnv.getLength(), equalTo(2));
+    }
+
+    @Test
+    void del() {
+        Variant del = SequenceVariant.of(chr1, 1, "AG", "A");
+
+        assertThat(del.getStartPosition(), equalTo(Position.of(1)));
+        assertThat(del.getEndPosition(), equalTo(Position.of(2)));
+        assertThat(del.getType(), equalTo(VariantType.DEL));
+        assertThat(del.getLength(), equalTo(2));
+    }
+
+    @Test
+    void ins() {
+        Variant ins = SequenceVariant.of(chr1, 1, "A", "AG");
+
+        assertThat(ins.getStartPosition(), equalTo(Position.of(1)));
+        assertThat(ins.getEndPosition(), equalTo(Position.of(1)));
+        assertThat(ins.getType(), equalTo(VariantType.INS));
+        assertThat(ins.getLength(), equalTo(1));
+    }
+
+    @Test
+    void insWithSameStrand() {
+        Variant ins = SequenceVariant.of(chr1, 1, "A", "AG");
+        assertSame(ins, ins.withStrand(Strand.POSITIVE));
+    }
+
+    @Test
+    void insWithNegativeStrand() {
+        Variant ins = SequenceVariant.of(chr1, 1, "A", "AG");
+        Variant negativeIns = ins.withStrand(Strand.NEGATIVE);
+
+        assertThat(negativeIns.getContig(), equalTo(chr1));
+        assertThat(negativeIns.getStrand(), equalTo(Strand.NEGATIVE));
+        assertThat(negativeIns.getStartPosition(), equalTo(Position.of(1000)));
+        assertThat(negativeIns.getEndPosition(), equalTo(Position.of(1000)));
+        assertThat(negativeIns.getRef(), equalTo("T"));
+        assertThat(negativeIns.getAlt(), equalTo("CT"));
+        assertThat(negativeIns.getType(), equalTo(VariantType.INS));
+        assertThat(negativeIns.getLength(), equalTo(1));
+    }
+
+    @Test
+    void snvToOppositeStrand() {
+        Variant snv = SequenceVariant.of(chr1, 1, "A", "T");
+        assertThat(snv.getStrand(), equalTo(Strand.POSITIVE));
+        Variant oppositeSnv = snv.toOppositeStrand();
+        assertThat(oppositeSnv.getContig(), equalTo(chr1));
+        assertThat(oppositeSnv.getStrand(), equalTo(Strand.NEGATIVE));
+        assertThat(oppositeSnv.getStartPosition(), equalTo(Position.of(1000)));
+        assertThat(oppositeSnv.getEndPosition(), equalTo(Position.of(1000)));
+        assertThat(oppositeSnv.getRef(), equalTo("T"));
+        assertThat(oppositeSnv.getAlt(), equalTo("A"));
+        assertThat(oppositeSnv.getType(), equalTo(VariantType.SNV));
+        assertThat(oppositeSnv.getLength(), equalTo(1));}
+
+    @Test
+    void symbolicVariantContainsSnv() {
+        Variant largeIns = SymbolicVariant.of(chr1, 1, 100, "T", "<INS>", 100, VariantType.INS);
+        assertTrue(largeIns.contains(SequenceVariant.of(chr1, 1, "A", "T")));
+        assertFalse(largeIns.contains(SequenceVariant.of(chr1, 200, "C", "A")));
+        assertTrue(largeIns.contains(PartialBreakend.of(chr1, Position.of(1), Strand.POSITIVE, "T")));
+    }
+
+}
