ca3c016faaf8ee47a7fddf00329ab0503a3777b5
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-11-11 18:17:12 +0000

    Remove id, mateId and VariantType from BreakendVariant constructor Add alt to BreakendVariant constructor

--- src/main/java/org/monarchinitiative/variant/api/impl/BreakendVariant.java
+++ src/main/java/org/monarchinitiative/variant/api/impl/BreakendVariant.java
@@ -9,18 +9,16 @@ import java.util.Objects;
  */
 public final class BreakendVariant implements Variant, Breakended {
 
-    private final String id;
-    private final String mateId;
     private final String eventId;
     private final Breakend left;
     private final Breakend right;
+    private final String ref;
     private final String alt;
-    private final VariantType variantType;
 
     // Accept the standard VCF input fields, including mateId and eventId
 
     // 1       28681758        gnomAD-SV_v2.1_BND_1_1117       N       <BND>   965     UNRESOLVED      END=28681759;SVTYPE=BND;SVLEN=1725;CHR2=1;POS2=28683483;END2=28683484;ALGORITHMS=delly,manta;EVIDENCE=PE,SR;UNRESOLVED_TYPE=SINGLE_ENDER_-+;
-
+    //
     // 2       4150333 28382_1 N       N]13:33423926]  284.1   PASS    SVTYPE=BND;POS=4150333;STRANDS=++:31;CIPOS=-8,10;CIEND=-2,1;CIPOS95=0,0;CIEND95=0,0;MATEID=28382_2;EVENT=28382;
     // 13      33423926        28382_2 N       N]2:4150333]    284.1   PASS    SVTYPE=BND;POS=33423926;STRANDS=++:31;CIPOS=-2,1;CIEND=-8,10;CIPOS95=0,0;CIEND95=0,0;MATEID=28382_1;EVENT=28382;
     // 2       135592459       28388_1 N       N[13:36400702[  2049.07 LOW     SVTYPE=BND;POS=135592459;STRANDS=+-:125;CIPOS=-2,10;CIEND=-9,9;CIPOS95=0,0;CIEND95=0,0;MATEID=28388_2;EVENT=28388;
@@ -28,25 +26,22 @@ public final class BreakendVariant implements Variant, Breakended {
     // 1       10567   2838_1  N       [15:102520406[N 47911.3 LOW     SVTYPE=BND;POS=10567;STRANDS=--:97;IMPRECISE;CIPOS=-205,24;CIEND=-104,42;CIPOS95=-20,20;CIEND95=-6,6;MATEID=2838_2;EVENT=2838;
     //15      102520406       2838_2  N       [1:10567[N      47911.3 LOW     SVTYPE=BND;POS=102520406;STRANDS=--:97;IMPRECISE;CIPOS=-104,42;CIEND=-205,24;CIPOS95=-6,6;CIEND95=-20,20;MATEID=2838_1;EVENT=2838;
 
-
-    public BreakendVariant(String id, String mateId, String eventId, Breakend left, Breakend right, String alt, VariantType variantType) {
-        this.id = id;
-        this.mateId = mateId;
+    public BreakendVariant(String eventId, Breakend left, Breakend right, String ref, String alt) {
         this.eventId = eventId;
         this.left = left;
         this.right = right;
+        this.ref = ref;
         this.alt = alt;
-        this.variantType = variantType;
     }
 
     @Override
     public String getId() {
-        return id;
+        return left.getId();
     }
 
     @Override
     public String getMateId() {
-        return mateId;
+        return right.getId();
     }
 
     @Override
@@ -86,7 +81,7 @@ public final class BreakendVariant implements Variant, Breakended {
 
     @Override
     public String getRef() {
-        return left.getRef();
+        return ref;
     }
 
     @Override
@@ -96,7 +91,7 @@ public final class BreakendVariant implements Variant, Breakended {
 
     @Override
     public VariantType getType() {
-        return variantType;
+        return VariantType.BND;
     }
 
     @Override
@@ -108,14 +103,14 @@ public final class BreakendVariant implements Variant, Breakended {
         if (left.getStrand().equals(strand)) {
             return this;
         } else {
-            return new BreakendVariant(id, mateId, eventId, right.toOppositeStrand(), left.toOppositeStrand(), Seq
-                    .reverseComplement(alt), variantType);
+            return new BreakendVariant(eventId, right.toOppositeStrand(), left.toOppositeStrand(), ref, Seq
+                    .reverseComplement(alt));
         }
     }
 
     @Override
     public BreakendVariant toOppositeStrand() {
-        return null;
+        return withStrand(getStrand().opposite());
     }
 
     @Override
@@ -128,30 +123,24 @@ public final class BreakendVariant implements Variant, Breakended {
         if (this == o) return true;
         if (!(o instanceof BreakendVariant)) return false;
         BreakendVariant that = (BreakendVariant) o;
-        return id.equals(that.id) &&
-                mateId.equals(that.mateId) &&
-                eventId.equals(that.eventId) &&
+        return eventId.equals(that.eventId) &&
                 left.equals(that.left) &&
                 right.equals(that.right) &&
-                alt.equals(that.alt) &&
-                variantType == that.variantType;
+                alt.equals(that.alt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, mateId, eventId, left, right, alt, variantType);
+        return Objects.hash(eventId, left, right, alt);
     }
 
     @Override
     public String toString() {
         return "BreakendVariant{" +
-                "id='" + id + '\'' +
-                ", mateId='" + mateId + '\'' +
-                ", eventId='" + eventId + '\'' +
+                "eventId='" + eventId + '\'' +
                 ", left=" + left +
                 ", right=" + right +
                 ", alt='" + alt + '\'' +
-                ", variantType=" + variantType +
                 '}';
     }
 }
--- src/test/java/org/monarchinitiative/variant/api/impl/BreakendVariantTest.java
+++ src/test/java/org/monarchinitiative/variant/api/impl/BreakendVariantTest.java
@@ -17,13 +17,13 @@ class BreakendVariantTest {
     @Test
     void matedBreakend() {
         // 2       4150333 28382_1 N       N]13:33423926]  284.1   PASS    SVTYPE=BND;POS=4150333;STRANDS=++:31;CIPOS=-8,10;CIEND=-2,1;CIPOS95=0,0;CIEND95=0,0;MATEID=28382_2;EVENT=28382;
-        Breakend left = PartialBreakend.of(chr2, Position.of(4150333, -8,10, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "N");
-        Breakend right = PartialBreakend.of(chr13, Position.of(33423926, -1,1, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "");
-        BreakendVariant instance = new BreakendVariant("28382_1", "28382_2", "28382", left, right, "", VariantType.BND);
+        Breakend left = PartialBreakend.of(chr2, Position.of(4150333, -8,10, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "28382_1");
+        Breakend right = PartialBreakend.of(chr13, Position.of(33423926, -2,1, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "28382_2");
+        BreakendVariant instance = new BreakendVariant("28382", left, right, "N", "N");
         assertThat(instance.getContig(), equalTo(chr2));
         assertThat(instance.getStartPosition().getPos(), equalTo(4150333));
         assertThat(instance.getRef(), equalTo("N"));
-        assertThat(instance.getAlt(), equalTo(""));
+        assertThat(instance.getAlt(), equalTo("N"));
         assertThat(instance.getLeft().getContig(), equalTo(chr2));
         assertThat(instance.getLeft().getPos(), equalTo(4150333));
         assertThat(instance.getRight().getContig(), equalTo(chr13));
@@ -31,10 +31,38 @@ class BreakendVariantTest {
 
         System.out.println(instance.withStrand(Strand.NEGATIVE));
         // 13      33423926        28382_2 N       N]2:4150333]    284.1   PASS    SVTYPE=BND;POS=33423926;STRANDS=++:31;CIPOS=-2,1;CIEND=-8,10;CIPOS95=0,0;CIEND95=0,0;MATEID=28382_1;EVENT=28382;
+        Breakend matedLeft = PartialBreakend.of(chr13, Position.of(33423926, -2,1, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "28382_2");
+        Breakend matedRight = PartialBreakend.of(chr2, Position.of(4150333, -8,10, CoordinateSystem.ONE_BASED), Strand.POSITIVE, "28382_1");
+        BreakendVariant matedInstance = new BreakendVariant("28382", matedLeft, matedRight, "N", "N");
 
         // these can be combined using their id and mateids to recreate the full SequenceRearrangement
-        // TODO: do we still need an adjacency or is a BreakendVariant OK? - there is a conflict with Adjacency::withStrand
-        // possibly best to have a toAdjacency() which can be used in creating a SequenceRearrangement?
     }
 
+    @Test
+    void insertedSequence() {
+        // #CHROM  POS  ID  REF  ALT  QUAL  FILTER  INFO
+        // 2  321682  bndV  T  ]13:123456]AGTNNNNNCAT  6  PASS  SVTYPE=BND;MATEID=bndU
+        // 13  123456  bndU  C  CAGTNNNNNCA[2:321682[  6  PASS  SVTYPE=BND;MATEID=bndV
+        Breakend left = PartialBreakend.of(chr2, Position.of(321682), Strand.POSITIVE, "bndV");
+        Breakend right = PartialBreakend.of(chr13, Position.of(123456), Strand.POSITIVE, "bndU");
+
+        BreakendVariant first = new BreakendVariant("", left, right, "T", "AGTNNNNNCAT");
+        System.out.println(first);
+        System.out.println(first.toOppositeStrand());
+        BreakendVariant second = new BreakendVariant("", right, left, "C", "CAGTNNNNNCA");
+    }
+
+    @Test
+    void positiveNegativeStrand() {
+        // 2       135592459       28388_1 N       N[13:36400702[  2049.07 LOW     SVTYPE=BND;POS=135592459;STRANDS=+-:125;CIPOS=-2,10;CIEND=-9,9;CIPOS95=0,0;CIEND95=0,0;MATEID=28388_2;EVENT=28388;
+        // 13      36400702        28388_2 N       ]2:135592459]N  2049.07 LOW     SVTYPE=BND;POS=36400702;STRANDS=+-:125;CIPOS=-9,9;CIEND=-2,10;CIPOS95=0,0;CIEND95=0,0;MATEID=28388_1;EVENT=28388;
+
+    }
+
+    @Test
+    void negativeNegativeStrand() {
+        // 1       10567   2838_1  N       [15:102520406[N 47911.3 LOW     SVTYPE=BND;POS=10567;STRANDS=--:97;IMPRECISE;CIPOS=-205,24;CIEND=-104,42;CIPOS95=-20,20;CIEND95=-6,6;MATEID=2838_2;EVENT=2838;
+        //15      102520406       2838_2  N       [1:10567[N      47911.3 LOW     SVTYPE=BND;POS=102520406;STRANDS=--:97;IMPRECISE;CIPOS=-104,42;CIEND=-205,24;CIPOS95=-6,6;CIEND95=-20,20;MATEID=2838_1;EVENT=2838;
+
+    }
 }
