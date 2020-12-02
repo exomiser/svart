a933f01ac3260027f1b636b0a6aec7a74baa5061
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-12-02 11:20:10 +0000

    Reformat CSV data in GenomicRegionTest for better readability

--- src/test/java/org/monarchinitiative/variant/api/GenomicRegionTest.java
+++ src/test/java/org/monarchinitiative/variant/api/GenomicRegionTest.java
@@ -61,11 +61,43 @@ public class GenomicRegionTest {
 
     @ParameterizedTest
     @CsvSource({
-            "ZERO_BASED, 2, 3, 0, 2, 3",
-            "ONE_BASED, 2, 3, 0, 2, 3",
-            "ZERO_BASED, 2, 3, 1, 1, 4",
-            "ONE_BASED, 2, 3, 1, 1, 4",
-            "ZERO_BASED, 2, 3, 2, 0, 5",
+            // source  start end  target     expected   pos
+            "POSITIVE, 3, 5,   POSITIVE,   POSITIVE,   3, 5",
+            "POSITIVE, 3, 5,   NEGATIVE,   NEGATIVE,   1, 3",
+            "POSITIVE, 3, 5,   UNSTRANDED, UNSTRANDED, 3, 5",
+            "POSITIVE, 3, 5,   UNKNOWN,    UNKNOWN,    3, 5",
+
+            "NEGATIVE, 3, 5,   POSITIVE,   POSITIVE,   1, 3",
+            "NEGATIVE, 3, 5,   NEGATIVE,   NEGATIVE,   3, 5",
+            "NEGATIVE, 3, 5,   UNSTRANDED, UNSTRANDED, 3, 5",
+            "NEGATIVE, 3, 5,   UNKNOWN,    UNKNOWN,    3, 5",
+
+            "UNSTRANDED, 3, 5, POSITIVE,   UNSTRANDED, 3, 5",
+            "UNSTRANDED, 3, 5, NEGATIVE,   UNSTRANDED, 3, 5",
+            "UNSTRANDED, 3, 5, UNSTRANDED, UNSTRANDED, 3, 5",
+            "UNSTRANDED, 3, 5, UNKNOWN,    UNKNOWN,    3, 5",
+
+            "UNKNOWN, 3, 5,    POSITIVE,   UNKNOWN,    3, 5",
+            "UNKNOWN, 3, 5,    NEGATIVE,   UNKNOWN,    3, 5",
+            "UNKNOWN, 3, 5,    UNSTRANDED, UNKNOWN,    3, 5",
+            "UNKNOWN, 3, 5,    UNKNOWN,    UNKNOWN,    3, 5"})
+    public void withStrand_strandConversions(Strand source, int start, int end, Strand target, Strand expected, int exptStart, int exptEnd) {
+        GenomicRegion initial = GenomicRegion.oneBased(chr1, source, Position.of(start), Position.of(end));
+
+        GenomicRegion actual = initial.withStrand(target);
+        assertThat(actual.strand(), equalTo(expected));
+        assertThat(actual.start(), equalTo(exptStart));
+        assertThat(actual.end(), equalTo(exptEnd));
+        assertThat(actual.coordinateSystem(), equalTo(initial.coordinateSystem()));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "ZERO_BASED, 2, 3,   0,   2, 3",
+            "ONE_BASED,  2, 3,   0,   2, 3",
+            "ZERO_BASED, 2, 3,   1,   1, 4",
+            "ONE_BASED,  2, 3,   1,   1, 4",
+            "ZERO_BASED, 2, 3,   2,   0, 5",
     })
     public void withPadding_singlePadding(CoordinateSystem coordinateSystem,
                                           int start, int end,
@@ -78,12 +110,12 @@ public class GenomicRegionTest {
 
     @ParameterizedTest
     @CsvSource({
-            "ZERO_BASED, 2, 3, 0, 0, 2, 3",
-            "ONE_BASED, 2, 3, 0, 0, 2, 3",
-            "ZERO_BASED, 2, 3, 1, 2, 1, 5",
-            "ONE_BASED, 2, 3, 1, 2, 1, 5",
-            "ZERO_BASED, 2, 3, 2, 0, 0, 3",
-            "ONE_BASED, 2, 3, 1, 0, 1, 3",
+            "ZERO_BASED, 2, 3,   0, 0,   2, 3",
+            "ONE_BASED,  2, 3,   0, 0,   2, 3",
+            "ZERO_BASED, 2, 3,   1, 2,   1, 5",
+            "ONE_BASED,  2, 3,   1, 2,   1, 5",
+            "ZERO_BASED, 2, 3,   2, 0,   0, 3",
+            "ONE_BASED,  2, 3,   1, 0,   1, 3",
     })
     public void withPadding_upDownPadding(CoordinateSystem coordinateSystem,
                                           int start, int end,
