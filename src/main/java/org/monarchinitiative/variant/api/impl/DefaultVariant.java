4ad9f42c53e2ce098708f352996eca5261b5aca3
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-12-17 16:27:54 +0000

    Update DefaultVariant to use BaseVariant methods for calculateChangeLength and calculateEnd

--- src/main/java/org/monarchinitiative/variant/api/impl/DefaultVariant.java
+++ src/main/java/org/monarchinitiative/variant/api/impl/DefaultVariant.java
@@ -4,34 +4,19 @@ import org.monarchinitiative.variant.api.*;
 
 public final class DefaultVariant extends BaseVariant<DefaultVariant> {
 
-
     private DefaultVariant(Contig contig, String id, Strand strand, CoordinateSystem coordinateSystem, Position startPosition, Position endPosition, String ref, String alt, int changeLength) {
         super(contig, id, strand, coordinateSystem, startPosition, endPosition, ref, alt, changeLength);
     }
 
     public static DefaultVariant of(Contig contig, String id, Strand strand, CoordinateSystem coordinateSystem, Position startPosition, Position endPosition, String ref, String alt, int changeLength) {
-        if (VariantType.isBreakend(alt)) {
-            throw new IllegalArgumentException("Unable to create variant from breakend allele " + alt);
-        }
+        assertNotBreakend(alt);
         return new DefaultVariant(contig, id, strand, coordinateSystem, startPosition, endPosition, ref, alt, changeLength);
     }
 
-
     public static DefaultVariant of(Contig contig, String id, Strand strand, CoordinateSystem coordinateSystem, Position start, String ref, String alt) {
-        if (VariantType.isSymbolic(alt)) {
-            throw new IllegalArgumentException("Unable to create non-symbolic variant from symbolic or breakend allele " + alt);
-        }
         Position end = calculateEnd(start, coordinateSystem, ref, alt);
-        int changeLength = alt.length() - ref.length();
+        int changeLength = calculateChangeLength(ref, alt);
         return new DefaultVariant(contig, id, strand, coordinateSystem, start, end, ref, alt, changeLength);
-    }
-
-    private static Position calculateEnd(Position start, CoordinateSystem coordinateSystem, String ref, String alt) {
-        // SNV case
-        if ((ref.length() | alt.length()) == 1) {
-            return coordinateSystem == CoordinateSystem.ZERO_BASED ? start.shift(1) : start;
-        }
-        return coordinateSystem == CoordinateSystem.ZERO_BASED ? start.withPos(start.pos() + ref.length()) : start.withPos(start.pos() + ref.length() - 1);
     }
 
     public static DefaultVariant oneBased(Contig contig, String id, int pos, String ref, String alt) {
