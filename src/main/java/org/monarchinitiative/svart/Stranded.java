c14dfd101c040527e2e2c55a669213de465460b9
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2023-05-22 19:11:25 +0100

    Add docs to Stranded

--- src/main/java/org/monarchinitiative/svart/Stranded.java
+++ src/main/java/org/monarchinitiative/svart/Stranded.java
@@ -1,6 +1,9 @@
 package org.monarchinitiative.svart;
 
 /**
+ * Marker interface to flag a class as having a {@link Strand}. If the class requires the ability to be transposed onto
+ * the opposite strand, then it should implement the {@link Transposable} interface.
+ *
  * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
  */
 public interface Stranded {
