e96d34665da9c6433002a15b829341318617e217
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2023-05-24 10:42:17 +0100

    Add GenomicVariantTest with explicit reference to SPDI paper for transposing zero-length insertions with interbase coordinates.

--- src/test/java/org/monarchinitiative/svart/GenomicVariantTest.java
+++ src/test/java/org/monarchinitiative/svart/GenomicVariantTest.java
@@ -1,8 +1,10 @@
 package org.monarchinitiative.svart;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.monarchinitiative.svart.assembly.GenomicAssemblies;
+import org.monarchinitiative.svart.util.VariantTrimmer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,15 +14,15 @@ class GenomicVariantTest {
     private static final Contig chr1 = GenomicAssemblies.GRCh38p13().contigById(1);
 
     @ParameterizedTest
-    @CsvSource ({
-        "100, 100, A, T, 0",
-        "100, 102, AGC, GCT, 0",
-        "100, 100, G, '', -1",
-        "100, 102, AGC, G, -2",
-        "100, 100, A, GCT, 2",
-        "100, 99, '', CT, 2",
-        "100, 200, A, <DEL>, -100",
-        "100, 100, A, 'A[2:321682[', 0",
+    @CsvSource({
+            "100, 100, A, T, 0",
+            "100, 102, AGC, GCT, 0",
+            "100, 100, G, '', -1",
+            "100, 102, AGC, G, -2",
+            "100, 100, A, GCT, 2",
+            "100, 99, '', CT, 2",
+            "100, 200, A, <DEL>, -100",
+            "100, 100, A, 'A[2:321682[', 0",
     })
     void staticConstructorTests(int start, int end, String ref, String alt, int changeLength) {
         // bleurgh! So many static scoped-constructors!
@@ -59,4 +61,26 @@ class GenomicVariantTest {
                 .build(), equalTo(expected));
     }
 
+    /**
+     * see <href <a href="https://academic.oup.com/bioinformatics/article/36/6/1902/5628222">SPDI: data model for variants and applications at NCBI</a>
+     * and supplementary material Table S1
+     * <p>
+     * 2.5 Alignment projection special cases
+     * <p>
+     * Two special cases must be considered carefully when projecting through alignments: alignment orientation changes
+     * and gaps in the alignment. The forward orientation of mRNA is determined by the direction of transcription (5′–3′)
+     * and the associated genes and their transcript products may be reverse to the chromosome. When mapping variants
+     * across such alignments, it is important to reverse-complement the inserted sequence, as the SPDI model uses only
+     * the positive strand. In addition, extra care must be taken when mapping an insertion variant with zero-length
+     * deletion sequence, because alignments use base coordinates, not SPDI’s interbase coordinates. It is usually
+     * convenient to convert to an insert-before semantic (which does not alter the numbering system) when computing the
+     * mapping. However, because directionality changes when strand orientation changes, insert-before becomes
+     * insert-after. In order to return to the insert-before semantic, the position must be increased by one (Fig. 2A).
+     */
+    @Test
+    void testZeroLengthInsertionStrandFlipChangesPosition() {
+        Contig chr1 = TestContig.of(1, 9);
+        GenomicVariant ins = GenomicVariant.of(chr1, Strand.POSITIVE, Coordinates.zeroBased(4, 4), "", "T");
+        assertThat(ins.startOnStrand(Strand.NEGATIVE), equalTo(5));
+    }
 }
