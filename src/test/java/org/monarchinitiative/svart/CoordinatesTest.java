3d0c2a1ffa4b66c8831b12181500b00eb96567da
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2023-06-27 09:41:19 +0100

    Improve test coverage of ImpreciseCoordinates

--- src/test/java/org/monarchinitiative/svart/CoordinatesTest.java
+++ src/test/java/org/monarchinitiative/svart/CoordinatesTest.java
@@ -12,7 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CoordinatesTest {
+class CoordinatesTest {
 
     @Test
     void emptyCoordinates() {
@@ -20,33 +20,61 @@ public class CoordinatesTest {
     }
 
     @Test
-    public void zeroBasedConstructorPrecise() {
+    void zeroBasedConstructorPrecise() {
         Coordinates instance = Coordinates.zeroBased(0, 10);
         assertThat(instance, equalTo(Coordinates.of(CoordinateSystem.zeroBased(), 0, ConfidenceInterval.precise(), 10, ConfidenceInterval.precise())));
         assertThat(instance.isPrecise(), is(true));
     }
 
     @Test
-    public void zeroBasedConstructorImprecise() {
+    void zeroBasedConstructorImprecise() {
         Coordinates instance = Coordinates.zeroBased(0, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise());
         assertThat(instance, equalTo(Coordinates.of(CoordinateSystem.zeroBased(), 0, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise())));
         assertThat(instance.isPrecise(), is(false));
     }
 
     @Test
-    public void oneBasedConstructorPrecise() {
+    void oneBasedConstructorPrecise() {
         Coordinates instance = Coordinates.oneBased(1, 10);
         assertThat(instance, equalTo(Coordinates.of(CoordinateSystem.oneBased(), 1, ConfidenceInterval.precise(), 10, ConfidenceInterval.precise())));
     }
 
     @Test
-    public void oneBasedConstructorImprecise() {
+    void oneBasedConstructorImprecise() {
         Coordinates instance = Coordinates.oneBased(1, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise());
         assertThat(instance, equalTo(Coordinates.of(CoordinateSystem.oneBased(), 1, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise())));
     }
 
+    @Nested
+    class Imprecise {
+
+        private final Coordinates instance = Coordinates.zeroBased(0, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise());
+
+        @Test
+        void impreciseToPrecise() {
+            assertThat(instance, equalTo(Coordinates.of(CoordinateSystem.zeroBased(), 0, ConfidenceInterval.of(0, 2), 10, ConfidenceInterval.precise())));
+            assertThat(instance.isPrecise(), is(false));
+            Coordinates asPrecise = instance.asPrecise();
+            assertThat(asPrecise, equalTo(Coordinates.of(CoordinateSystem.zeroBased(), 0, 10)));
+            assertThat(asPrecise.isPrecise(), is(true));
+        }
+
+        @Test
+        void withCoordinateSystem() {
+            assertThat(instance.coordinateSystem(), equalTo(CoordinateSystem.ZERO_BASED));
+            assertThat(instance.withCoordinateSystem(CoordinateSystem.ZERO_BASED).coordinateSystem(), equalTo(CoordinateSystem.ZERO_BASED));
+            assertThat(instance.withCoordinateSystem(CoordinateSystem.ONE_BASED).coordinateSystem(), equalTo(CoordinateSystem.ONE_BASED));
+        }
+
+        @Test
+        void extend() {
+            assertThat(instance.extend(0, 0), equalTo(instance));
+            assertThat(instance.extend(-5, 10), equalTo(Coordinates.zeroBased(5, ConfidenceInterval.of(0, 2), 20, ConfidenceInterval.precise())));
+        }
+    }
+    
     @Test
-    public void testStartTypes() {
+    void testStartTypes() {
         Coordinates oneBasedCoords = Coordinates.oneBased(1, 10);
         Coordinates zeroBasedCoords = Coordinates.zeroBased(0, 10);
         assertThat(oneBasedCoords.start(), not(equalTo(zeroBasedCoords.start())));
@@ -61,7 +89,7 @@ public class CoordinatesTest {
             "ZERO_BASED,  100,  A,   101",
             "ZERO_BASED,  100,  AT,  102",
     })
-    public void fromAllele(CoordinateSystem coordinateSystem, int start, String ref, int expectEnd) {
+    void fromAllele(CoordinateSystem coordinateSystem, int start, String ref, int expectEnd) {
         Coordinates fromAllele = Coordinates.ofAllele(coordinateSystem, start, ref);
         Coordinates expected = Coordinates.of(coordinateSystem, start, expectEnd);
         assertEquals(expected, fromAllele);
@@ -72,7 +100,7 @@ public class CoordinatesTest {
             "ONE_BASED,   1",
             "ZERO_BASED,  0",
     })
-    public void breakend(CoordinateSystem coordinateSystem, int start) {
+    void breakend(CoordinateSystem coordinateSystem, int start) {
         Coordinates breakend = Coordinates.ofBreakend(coordinateSystem, start, ConfidenceInterval.precise());
         assertThat(breakend.length(), equalTo(0));
     }
@@ -87,7 +115,7 @@ public class CoordinatesTest {
             "ZERO_BASED,  0, 1,  1",
             "ZERO_BASED,  0, 2,  2",
     })
-    public void length(CoordinateSystem coordinateSystem, int start, int end, int expected) {
+    void length(CoordinateSystem coordinateSystem, int start, int end, int expected) {
         assertThat(Coordinates.length(coordinateSystem, start, end), equalTo(expected));
     }
 
@@ -107,7 +135,7 @@ public class CoordinatesTest {
             "ZERO_BASED,  4,  1",
             "ZERO_BASED,  5,  0",
     })
-    public void invert(CoordinateSystem coordinateSystem, int pos, int expected) {
+    void invert(CoordinateSystem coordinateSystem, int pos, int expected) {
         Contig contig = TestContig.of(1, 5);
         assertThat(Coordinates.invertCoordinate(coordinateSystem, contig, pos), equalTo(expected));
     }
@@ -128,7 +156,7 @@ public class CoordinatesTest {
             "ZERO_BASED,  4,  5,  0,  1",
             "ZERO_BASED,  2,  4,  1,  3",
     })
-    public void invertCoordinates(CoordinateSystem coordinateSystem, int start, int end, int exptStart, int exptEnd) {
+    void invertCoordinates(CoordinateSystem coordinateSystem, int start, int end, int exptStart, int exptEnd) {
         Contig contig = TestContig.of(1, 5);
         Coordinates initial = Coordinates.of(coordinateSystem, start, end);
         Coordinates expected = Coordinates.of(coordinateSystem, exptStart, exptEnd);
@@ -136,7 +164,7 @@ public class CoordinatesTest {
     }
 
     @Nested
-    public class Overlap {
+    class Overlap {
         @ParameterizedTest
         @CsvSource({
                 "ONE_BASED,   1, 1,  ONE_BASED,   1, 1,    true",
@@ -149,7 +177,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  1, 5,  ZERO_BASED,  5, 8,    false",
                 "ZERO_BASED,  0, 1,  ONE_BASED,   1, 1,    true",
         })
-        public void overlap(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
+        void overlap(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
             assertThat(Coordinates.overlap(x, xStart, xEnd, y, yStart, yEnd), equalTo(expected));
         }
 
@@ -162,7 +190,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 0,  ONE_BASED,   1, 0,  true",
                 "ZERO_BASED,  0, 0,  ZERO_BASED,  0, 0,  true",
         })
-        public void overlap_emptyRegions(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
+        void overlap_emptyRegions(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
             assertThat(Coordinates.overlap(x, xStart, xEnd, y, yStart, yEnd), equalTo(expected));
         }
 
@@ -179,7 +207,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 0,  ZERO_BASED,  0, 1,    false",
                 "ZERO_BASED,  1, 1,  ZERO_BASED,  0, 1,    false",
         })
-        public void overlap_excludesEmptyIntervalsOnBoundaries(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
+        void overlap_excludesEmptyIntervalsOnBoundaries(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
             assertThat(Coordinates.overlap(x, xStart, xEnd, y, yStart, yEnd), equalTo(expected));
         }
 
@@ -193,13 +221,13 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 5,  ZERO_BASED,  3, 8,    true",
                 "ZERO_BASED,  3, 8,  ZERO_BASED,  0, 5,    true",
         })
-        public void overlap_isTransitive(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
+        void overlap_isTransitive(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, boolean expected) {
             assertThat(Coordinates.overlap(x, xStart, xEnd, y, yStart, yEnd), equalTo(expected));
         }
     }
 
     @Nested
-    public class Validate {
+    class Validate {
 
         @ParameterizedTest
         @CsvSource({
@@ -212,7 +240,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0,  1",
                 "ZERO_BASED,  0,  5",
         })
-        public void validCoordinates(CoordinateSystem coordinateSystem, int start, int end) {
+        void validCoordinates(CoordinateSystem coordinateSystem, int start, int end) {
             assertDoesNotThrow(() -> Coordinates.validateCoordinates(coordinateSystem, start, end));
         }
 
@@ -229,7 +257,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0,  -1",
                 "ZERO_BASED,  -1,  0",
         })
-        public void invalidCoordinates(CoordinateSystem coordinateSystem, int start, int end) {
+        void invalidCoordinates(CoordinateSystem coordinateSystem, int start, int end) {
             assertThrows(InvalidCoordinatesException.class, () -> Coordinates.validateCoordinates(coordinateSystem, start, end));
         }
     }
@@ -239,12 +267,12 @@ public class CoordinatesTest {
         "ONE_BASED,   -1",
         "ZERO_BASED,   0",
     })
-    public void endDelta(CoordinateSystem coordinateSystem, int expected) {
+    void endDelta(CoordinateSystem coordinateSystem, int expected) {
         assertThat(Coordinates.endDelta(coordinateSystem), equalTo(expected));
     }
 
     @Nested
-    public class DistanceTo {
+    class DistanceTo {
 
         @ParameterizedTest
         @CsvSource({
@@ -255,7 +283,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 2,   ONE_BASED,   2, 3,    0",
                 "ZERO_BASED,  0, 2,   ZERO_BASED,  1, 3,    0",
         })
-        public void overlapping(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
+        void overlapping(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
             assertThat(Coordinates.distanceAToB(x, xStart, xEnd, y, yStart, yEnd), is(expected));
             assertThat(Coordinates.distanceAToB(y, yStart, yEnd, x, xStart, xEnd), is(-expected));
         }
@@ -269,7 +297,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 2,   ONE_BASED,   3, 4,    0",
                 "ZERO_BASED,  0, 2,   ZERO_BASED,  2, 4,    0",
         })
-        public void adjacent(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
+        void adjacent(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
             assertThat(Coordinates.distanceAToB(x, xStart, xEnd, y, yStart, yEnd), is(expected));
             assertThat(Coordinates.distanceAToB(y, yStart, yEnd, x, xStart, xEnd), is(-expected));
         }
@@ -283,7 +311,7 @@ public class CoordinatesTest {
                 "ZERO_BASED,  0, 2,   ONE_BASED,   5, 6,    2",
                 "ZERO_BASED,  0, 2,   ZERO_BASED,  4, 6,    2",
         })
-        public void distanceIsTwo(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
+        void distanceIsTwo(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
             assertThat(Coordinates.distanceAToB(x, xStart, xEnd, y, yStart, yEnd), is(expected));
             assertThat(Coordinates.distanceAToB(y, yStart, yEnd, x, xStart, xEnd), is(-expected));
         }
@@ -313,7 +341,7 @@ public class CoordinatesTest {
             "ZERO_BASED, 0, 3,   ONE_BASED,  2, 3,    2",
             "ZERO_BASED, 0, 2,   ZERO_BASED, 1, 3,    1",
     })
-    public void overlapLength(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
+    void overlapLength(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
         assertThat(Coordinates.overlapLength(x, xStart, xEnd, y, yStart, yEnd), equalTo(expected));
     }
 
@@ -341,12 +369,12 @@ public class CoordinatesTest {
             "ZERO_BASED, 0, 3,   ONE_BASED,  2, 3,    2",
             "ZERO_BASED, 0, 2,   ZERO_BASED, 1, 3,    1",
     })
-    public void overlapLengthCoordinates(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
+    void overlapLengthCoordinates(CoordinateSystem x, int xStart, int xEnd, CoordinateSystem y, int yStart, int yEnd, int expected) {
         assertThat(Coordinates.of(x, xStart, xEnd).overlapLength(Coordinates.of(y, yStart, yEnd)), equalTo(expected));
     }
 
     @Test
-    public void comparatorNaturalOrderTest() {
+    void comparatorNaturalOrderTest() {
         Coordinates first = Coordinates.of(CoordinateSystem.zeroBased(), 0, 5);
         Coordinates second = Coordinates.of(CoordinateSystem.oneBased(), 2, 5);
         Coordinates third = Coordinates.of(CoordinateSystem.zeroBased(), 2, 4);
@@ -356,7 +384,7 @@ public class CoordinatesTest {
     }
 
     @Test
-    public void comparableTest() {
+    void comparableTest() {
         Coordinates first = Coordinates.of(CoordinateSystem.zeroBased(), 0, 5);
         Coordinates second = Coordinates.of(CoordinateSystem.oneBased(), 2, 5);
         Coordinates third = Coordinates.of(CoordinateSystem.zeroBased(), 2, 4);
